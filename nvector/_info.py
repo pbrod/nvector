da5636391c4f38e362336349a10c6abd40dd4d87
Per A Brodtkorb <per.andreas.brodtkorb@gmail.com>
2017-02-03 15:42:26 +0100

    Updated installation information in _info.py

--- nvector/_info.py
+++ nvector/_info.py
@@ -66,6 +66,15 @@ If you have pip installed, then simply type:
 to get the lastest stable version. Using pip also has the advantage that all
 requirements are automatically installed.
 
+You can download nvector and all dependencies to a folder "pkg", by the following:
+
+   $ pip install --download=pkg nvector
+
+To install the downloaded nvector, just type:
+
+   $ pip install --no-index --find-links=pkg nvector
+
+
 Unit tests
 ===========
 To test if the toolbox is working paste the following in an interactive
