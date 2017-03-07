b5e3087ceb11d89e36403414dc4df529aa8f0394
Per A Brodtkorb <per.andreas.brodtkorb@gmail.com>
2017-03-07 13:16:21 +0100

    Updated crosstrack distance tests.

--- nvector/tests/test_nvector.py
+++ nvector/tests/test_nvector.py
@@ -334,36 +334,27 @@ class TestNvector(unittest.TestCase):
         assert_array_almost_equal(s_xt, 11117.79911015)
         assert_array_almost_equal(d_xt, 11117.79346741)
 
-    @staticmethod
-    def test_small_cross_track_distance():
+    def test_small_and_large_cross_track_distance(self):
         radius = 6371e3  # m, mean Earth radius
         n_EA1_E = lat_lon2n_E(rad(0), rad(0))
         n_EA2_E = lat_lon2n_E(rad(10), rad(0))
         n_EB0_E = lat_lon2n_E(rad(1), rad(0.1))
         n_EB1_E = closest_point_on_great_circle((n_EA1_E, n_EA2_E), n_EB0_E)
-        s_xt0 = np.pi/2*6371e+3+1
-        distance_rad = s_xt0 / radius
-        n_EB_E = n_EA_E_distance_and_azimuth2n_EB_E(n_EB1_E, distance_rad,
-                                                    np.pi/2)
 
-        n_EB2_E = closest_point_on_great_circle((n_EA1_E, n_EA2_E), n_EB_E)
+        for s_xt0 in [np.pi/3 * radius, 10., 0.1, 1e-4, 1e-8]:
+            distance_rad = s_xt0 / radius
+            n_EB_E = n_EA_E_distance_and_azimuth2n_EB_E(n_EB1_E, distance_rad,
+                                                        np.pi/2)
 
-        # Find the cross track distance from path A to position B.
-
-        # SOLUTION:
-        # Find the unit normal to the great circle:
-        c_E = unit(np.cross(n_EA1_E, n_EA2_E, axis=0))
-
-        # Find the great circle cross track distance:
-        sin_theta = -np.dot(c_E.T, n_EB_E)
-        n_c = np.cross(c_E, n_EB_E, axis=0)
-        t = np.sign(np.dot(n_c.T, n_EA1_E))
-        cos_theta = -t*np.linalg.norm(n_c, axis=0)
-        s_xt = np.arcsin(sin_theta) * radius
-        s_xt2 = (np.arccos(np.dot(c_E.T, n_EB_E)) - np.pi/2) * radius
-        s_xt3 = great_circle_distance(n_EB1_E, n_EB_E, radius)
-        s_xt4 = np.arctan2(sin_theta, cos_theta) * radius
-        pass
+            n_EB2_E = closest_point_on_great_circle((n_EA1_E, n_EA2_E), n_EB_E)
+            s_xt = great_circle_distance(n_EB1_E, n_EB_E, radius)
+            c_E = unit(np.cross(n_EA1_E, n_EA2_E, axis=0))
+            s_xt2 = (np.arccos(np.dot(c_E.T, n_EB_E)) - np.pi/2) * radius
+            assert_array_almost_equal(n_EB2_E, n_EB1_E)
+            assert_array_almost_equal(s_xt, s_xt0)
+            assert_array_almost_equal(s_xt2, s_xt0)
+            self.assertTrue(np.abs(s_xt-s_xt0)/s_xt0 < 1e-15, 's_xt fails')
+            # self.assertTrue(np.abs(s_xt2-s_xt0)/s_xt0 < 1e-12, 's_xt2 fails')
 
     @staticmethod
     def test_R2xyz():
