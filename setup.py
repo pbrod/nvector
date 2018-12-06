f92e0c63519f21ee29d112a28b115cfcd49f80dd
Per A Brodtkorb <per.andreas.brodtkorb@gmail.com>
2018-12-06 01:43:32 +0100

    I have reset the setup.py to require 'pyscaffold==2.5.11' which works on python version 3.4, 3.5 and 3.6. as well as 2.7

--- setup.py
+++ setup.py
@@ -45,28 +45,73 @@ Delete the build, dist, and nvector.egg-info folder in your root directory.
 """
 
 import sys
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
-def print_version():
+def _get_version_from_pkg():
     import pkg_resources
     try:
-        __version__ = pkg_resources.get_distribution("nvector").version
+        version = pkg_resources.get_distribution("nvector").version
         with open("__conda_version__.txt", "w") as fid:
-            fid.write(__version__)
+            fid.write(version)
     except pkg_resources.DistributionNotFound:
-        __version__ = 'unknown'
-    print("Version: {0}".format(__version__))
+        version = 'unknown'
+    return version
+
+
+def _get_version_from_git():
+    import subprocess
+    try:
+        version = subprocess.check_output("git describe --tags").decode('utf-8')
+        version = version.lstrip('v').strip()
+    except subprocess.CalledProcessError:
+        version = 'unknown'
+    parts = version.split('-')
+    if len(parts) == 1:
+        version = parts[0]
+    elif len(parts) == 3:
+        tag, revision, sha = parts
+        version = '{}.post{:03d}+{}'.format(tag, int(revision), sha)
+    else:
+        version = 'unknown'
+    return version
+
+
+def get_version():
+    version = _get_version_from_git()
+    if version == 'unknown':
+        return _get_version_from_pkg()
+    return version
+
+
+def update_version_in_package(version):
+    import re
+    if version != 'unknown':
+        with open("./src/nvector/__init__.py", "r") as fid:
+            text = fid.read()
+
+        new_text = re.sub(r"__version__ = ['\"]([^'\"]*)['\"]",
+                          '__version__ = "{}"'.format(version),
+                          text, re.M)
+
+        with open("./src/nvector/__init__.py", "w") as fid:
+            fid.write(new_text)
 
 
 def setup_package():
+    version = get_version()
+    update_version_in_package(version)
+    print("Version: {}".format(version))
+
     needs_sphinx = {'build_sphinx', 'upload_docs'}.intersection(sys.argv)
     sphinx = ['sphinx', 'numpydoc',
               'sphinx_rtd_theme>=0.1.7'] if needs_sphinx else []
-    setup(setup_requires=['six', 'pyscaffold>=2.4rc1,<2.5a0'] + sphinx,
+    setup(setup_requires=['pyscaffold==2.5.11'] + sphinx,
+          package_dir = {'': 'src'},
+          include_package_data=True,
+          packages=find_packages(where=r'./src'),
           tests_require=['pytest_cov', 'pytest'],
           use_pyscaffold=True)
-    print_version()
 
 
 if __name__ == "__main__":
