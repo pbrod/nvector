badb57989e807c76dc64485087d924ce7bf6ed2b
Per A Brodtkorb <per.andreas.brodtkorb@gmail.com>
2021-09-16 14:16:36 +0200

    Updated badges for Travis testserver and downloads in README.rst and _images.py

--- README.rst
+++ README.rst
@@ -594,8 +594,8 @@ to do the calculations.
 
 .. |pkg_img| image:: https://badge.fury.io/py/nvector.png
    :target: https://pypi.python.org/pypi/nvector/
-.. |tests_img| image:: https://travis-ci.com/pbrod/Nvector.svg?branch=master
-   :target: https://travis-ci.com/pbrod/Nvector
+.. |tests_img| image:: https://app.travis-ci.com/pbrod/Nvector.svg?branch=master
+   :target: https://app.travis-ci.com/pbrod/Nvector
 .. |docs_img| image:: https://readthedocs.org/projects/pip/badge/?version=stable
    :target: http://Nvector.readthedocs.org/en/stable/
 .. |health_img| image:: https://api.codeclimate.com/v1/badges/c04214bef610b25906fe/maintainability
@@ -605,7 +605,8 @@ to do the calculations.
    :target: https://codecov.io/gh/pbrod/nvector
 .. |versions_img| image:: https://img.shields.io/pypi/pyversions/Nvector.svg
    :target: https://github.com/pbrod/nvector
-.. |downloads_img| image:: https://img.shields.io/pypi/dm/nvector.svg
+.. |downloads_img| image:: https://pepy.tech/badge/nvector/month
+   :target: https://pepy.tech/project/nvector
    :alt: PyPI - Downloads
 
 
--- src/nvector/_images.py
+++ src/nvector/_images.py
@@ -3,18 +3,24 @@
 
     .. |pkg_img| image:: https://badge.fury.io/py/nvector.png
        :target: https://pypi.python.org/pypi/nvector/
-    .. |tests_img| image:: https://travis-ci.com/pbrod/Nvector.svg?branch=master
-       :target: https://travis-ci.com/pbrod/Nvector
+
+    .. |tests_img| image:: https://app.travis-ci.com/pbrod/Nvector.svg?branch=master
+        :target: https://app.travis-ci.com/pbrod/Nvector
+
     .. |docs_img| image:: https://readthedocs.org/projects/pip/badge/?version=stable
        :target: http://Nvector.readthedocs.org/en/stable/
+
     .. |health_img| image:: https://api.codeclimate.com/v1/badges/c04214bef610b25906fe/maintainability
        :target: https://codeclimate.com/github/pbrod/Nvector/maintainability
        :alt: Maintainability
+
     .. |coverage_img| image:: https://codecov.io/gh/pbrod/nvector/branch/master/graph/badge.svg
        :target: https://codecov.io/gh/pbrod/nvector
+
     .. |versions_img| image:: https://img.shields.io/pypi/pyversions/Nvector.svg
        :target: https://github.com/pbrod/nvector
-    .. |downloads_img| image:: https://img.shields.io/pypi/dm/nvector.svg
-       :alt: PyPI - Downloads
 
+    .. |downloads_img| image:: https://pepy.tech/badge/nvector/month
+       :target: https://pepy.tech/project/nvector
+       :alt: PyPI - Downloads
 """
