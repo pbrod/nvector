92a859f660f47ffdb566fad0453df137c3b7d163
Per A Brodtkorb <per.andreas.brodtkorb@gmail.com>
2026-08-15 17:20:56 +0200

    refactor(__init__.py): replace wildcard imports with explicit imports

--- src/nvector/__init__.py
+++ src/nvector/__init__.py
@@ -3,11 +3,76 @@ from typing import Any
 
 from ._common import use_docstring
 from ._info import __doc__ as __doc__  # @UnresolvedImport
-from .core import *  # noqa
-from .objects import *  # noqa
-from .rotation import *  # noqa
+from .core import (
+    closest_point_on_great_circle,
+    course_over_ground,
+    cross_track_distance,
+    euclidean_distance,
+    geodesic_distance,
+    geodesic_reckon,
+    great_circle_distance,
+    great_circle_distance_rad,
+    great_circle_normal,
+    interp_nvectors,
+    interpolate,
+    intersect,
+    lat_lon2n_E,
+    mean_horizontal_position,
+    n_E2lat_lon,
+    n_EA_E_and_n_EB_E2azimuth,
+    n_EA_E_and_n_EB_E2p_AB_E,
+    n_EA_E_and_n_EB_E2p_AB_N,
+    n_EA_E_and_p_AB_E2n_EB_E,
+    n_EA_E_and_p_AB_N2n_EB_E,
+    n_EA_E_distance_and_azimuth2n_EB_E,
+    n_EB_E2p_EB_E,
+    on_great_circle,
+    on_great_circle_path,
+    p_EB_E2n_EB_E,
+)
+from .objects import (
+    ECEFvector,
+    FrameB,
+    FrameE,
+    FrameL,
+    FrameN,
+    GeoPath,
+    GeoPoint,
+    Nvector,
+    Pvector,
+    delta_E,
+    delta_L,
+    delta_N,
+)
+from .rotation import (
+    E_rotation,
+    R2xyz,
+    R2zyx,
+    R_EL2n_E,
+    R_EN2n_E,
+    change_axes_to_E,
+    n_E2R_EN,
+    n_E_and_wa2R_EL,
+    xyz2R,
+    zyx2R,
+)
 from .testing import test as _test  # noqa
-from .util import *  # noqa
+from .util import (
+    allclose,
+    array_to_list_dict,
+    deg,
+    degrees2dm,
+    dm2degrees,
+    eccentricity2,
+    get_ellipsoid,
+    isclose,
+    mdot,
+    nthroot,
+    polar_radius,
+    rad,
+    third_flattening,
+    unit,
+)
 
 __version__ = "1.1.0"
 
